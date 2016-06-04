1a6d67a0ab7728db291f315de88c6abb9f256d3d
tonyyasi <tonyyasi@gmail.com>
2016-06-03 22:30:06 -0500

    Testing some stuff out

--- 06/read.rb
+++ 06/read.rb
@@ -3,46 +3,26 @@
 # gem intall rack
 # Resources:
 # http://rack.github.io/
+require 'ostruct'
 
 
-class GithubUser
-	attr_reader :url, :username, :name , :imagen , :location
+class GithubUser < OpenStruct
+	
+def self.find(username)
 
-	def initialize(url, username, name, imagen, location)
-	@name = name
-	@url = url
-	@username = username 
-	@imagen = imagen
-	@location = location
+raw_user = RestClient.get "https://api.github.com/users/#{username}"
+github_user = JSON.parse(raw_user)
+
+self.new(github_user)
+end
 	
 
-	end
-
-	def self.initialize_all_users
-		all_users = []
-		raw_users = RestClient.get 'https://api.github.com/users?since=135'
-		github_user = JSON.parse(raw_users)
-		github_user.each do |user|
-			all_users << GithubUser.new(github_user["html_url"], github_user["login"], github_user["name"], github_user["avatar_url"], github_user["location"])
-		end
-		all_users
-
-	end
+	
 end
 
 
 require 'rest-client'
 require 'json'
-
-
-
-raw_user = RestClient.get 'https://api.github.com/users/tonyyasi'
-github_user = JSON.parse(raw_user)
-#puts github_user
-
-
-
-
 require 'rack'
 
 ## The most basic web app
@@ -63,18 +43,18 @@ class Page
     ERB.new(File.read('./layout.erb')).result(binding)
   end
 
+
   def content
-  	raw_user = RestClient.get 'https://api.github.com/users/tonyyasi'
-github_user = JSON.parse(raw_user)
-  	user = GithubUser.new(github_user["html_url"], github_user["login"], github_user["name"], github_user["avatar_url"], github_user["location"])
+  	
+  	user = GithubUser.find("tonyyasi")
     render do
       %Q{
       	<h1> Name: #{user.name}  </h1>
-      	<img src="#{user.imagen}" style="width:304px;height:328px;">
+      	<img src="#{user.avatar_url}" style="width:304px;height:328px;">
       	<h2> Stuff about this guy </h2>
       	<ul>
-      		<li> Github username: #{user.username} </li>
-      		<li>URL:  #{user.url} </li>
+      		<li> Github username: #{user.login} </li>
+      		<li>URL:  <a href ="#{user.html_url}">#{user.html_url} </a></li>
       		<li>Hobbies: Music, programming and stuff </li>
       	</ul>
       	<p>Favorite programming languages</p>
@@ -87,12 +67,12 @@ github_user = JSON.parse(raw_user)
       	</ol>
 
       	<p>
-      	Follow me on twitter
-      	<a href="www.twitter.com/tonyyasi"><img src="https://lh3.ggpht.com/lSLM0xhCA1RZOwaQcjhlwmsvaIQYaP3c5qbDKCgLALhydrgExnaSKZdGa8S3YtRuVA=w300" style ="width:200px;height:200px;" ></a>
+      	<p>Follow me on twitter<p>
+      	<a href="https://twitter.com/tonyyasi"><img src="https://lh3.ggpht.com/lSLM0xhCA1RZOwaQcjhlwmsvaIQYaP3c5qbDKCgLALhydrgExnaSKZdGa8S3YtRuVA=w300" style ="width:200px;height:200px;" ></a>
       	</p>
       	<p>
-      	Checkout Icalia
-      	<a href="http://www.icalialabs.com/"><img src="https://pbs.twimg.com/profile_images/523198552063746048/BgFH_zPm_400x400.png"></a>
+      	<p>Checkout Icalia</p>
+      	<a href="http://www.icalialabs.com/"><img src="https://pbs.twimg.com/profile_images/523198552063746048/BgFH_zPm_400x400.png" style="width:200px;height:200px;"></a>
       	</p>
       }
     end
