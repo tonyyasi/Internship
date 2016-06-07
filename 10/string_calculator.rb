f89727c59640f389fa4771e19e94c1bcbab856bd
tonyyasi <tonyyasi@gmail.com>
2016-06-07 11:15:24 -0500

    A bit of refactoring

--- 10/string_calculator.rb
+++ 10/string_calculator.rb
@@ -1,69 +1,37 @@
 class StringCalculator
 
 	def numeric?(lookAhead)
-  lookAhead =~ /[[:digit:]]/
-end
-
-
+		lookAhead =~ /[[:digit:]]/
+	end
 
 	def add(string)
-		if string.size == 0
-			return 0 
-
-		end
+		return 0 if string.empty?
 
 		arr = []
 		negatives = []
-		temporal = ''
-		# arr = []
-		# arr = string.gsub(/[\/n]/,',').split(',')
-		# puts arr.inspect
-
-		# arr.map(&:to_i).reduce(:+)
-
 		temporal_string = ''
 		string = string.gsub(/[\/n]/,',')
-		#puts string
 
 		string.each_char.with_index do |char,index|
-			#puts char
 
-			if(numeric?(char))
-			temporal_string = temporal_string + char 
+			if numeric?(char)
+				temporal_string = temporal_string + char 
+			else
+				arr.push(temporal_string) if temporal_string != ''
+				temporal_string = ''
+			end
 
-		else
-			#puts temporal_string
-			arr.push(temporal_string) if temporal_string != ''
-			temporal_string = ''
-
+			if char == '-' && numeric?(string[index+1])
+				negatives.push("-#{string[index+1]}")
+			end
 		end
 
-		if(char == '-' && numeric?(string[index+1]))
-			negatives.push("-#{string[index+1]}")
-
-		end
-
-
-
-		end
 		arr.push(temporal_string)
-
-		puts arr.inspect
-
 		
-		if(negatives.empty?)
+		if negatives.empty?
 			arr.map(&:to_i).reduce(:+)
 		else
-
-			  "No negatives allowed " + "#{negatives.map{ |i|  %Q(#{i}) }.join(' ')}"
+			"No negatives allowed " + "#{negatives.join(' ')}"
 		end
-
-
-		
-
 	end
-
 end
-
-
-
