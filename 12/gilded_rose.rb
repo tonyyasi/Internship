182c28a8d6c476264b80db106bb9d35339e88aa9
tonyyasi <tonyyasi@gmail.com>
2016-06-08 12:03:35 -0500

    created class game

--- 12/gilded_rose.rb
+++ 12/gilded_rose.rb
@@ -5,15 +5,22 @@ class GildedRose
     @name, @quality, @days_remaining = name, quality, days_remaining
   end
 
+  def tick
+    return normal_tick if @name == "normal"
+    return brie_tick if @name == "Aged Brie"
+    return sulfuras_tick if @name == "Sulfuras, Hand of Ragnaros"
+    return backstage_tick if @name == "Backstage passes to a TAFKAL80ETC concert"
+  end
+
+private
+
   def normal_tick
     if @quality != 0
       if @days_remaining <= 0
         @quality -= 1
       end
-
       @quality -= 1
     end
-
     @days_remaining -= 1
   end
 
@@ -25,31 +32,16 @@ class GildedRose
 
   def sulfuras_tick
 
-
-
   end
 
   def backstage_tick
-
-    
+ 
     @quality = 0 if @days_remaining <= 0 
     @quality +=1 if @days_remaining == 11 && @quality < 50
     @quality += 2 if @days_remaining > 5 && @days_remaining <11
     @quality += 3 if @days_remaining <= 5 && @days_remaining > 0
-
-    
-    
-
     @days_remaining -= 1
 
   end
 
-  def tick
-    return normal_tick if @name == "normal"
-    return brie_tick if @name == "Aged Brie"
-    return sulfuras_tick if @name == "Sulfuras, Hand of Ragnaros"
-    return backstage_tick if @name == "Backstage passes to a TAFKAL80ETC concert"
-  end
-
 end
-
