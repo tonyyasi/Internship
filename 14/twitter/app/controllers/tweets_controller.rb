c16f67b6fee189341ff8ed13063d2be2921b68db
tonyyasi <tonyyasi@gmail.com>
2016-06-11 17:10:58 -0500

    Even more changes

---
+++ 14/twitter/app/controllers/tweets_controller.rb
@@ -0,0 +1,60 @@
+class TweetsController < ApplicationController
+
+def index
+  @tweets = Tweet.all
+  @tweet = Tweet.new
+
+end
+
+def destroy
+  @tweet = Tweet.find(params[:id])
+ @tweet.destroy
+   respond_to do |format|
+   #format.html {redirect_to tweet}
+   format.js
+   end
+  
+end
+
+def show
+ @tweet = Tweet.find(params[:id])
+
+end
+
+def new
+  @tweet = Tweet.new
+end
+
+def create
+ @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      respond_to do |format|
+        format.html { redirect_to @tweet }
+        format.js
+      end
+    else
+      render :new
+   end
+end
+
+def edit
+ @tweet = Tweet.find(params[:id])
+end
+
+def update
+  @tweet = Tweet.find(params[:id])
+ @tweet.content = tweet_params[:content]
+ if @tweet.save
+   redirect_to tweet_path(tweet)
+ else
+  render :edit
+ end
+end
+
+private
+
+def tweet_params
+  params.require(:tweet).permit(:content)
+end
+
+end
--- 14/twitter/config/routes.rb
+++ 14/twitter/config/routes.rb
@@ -8,9 +8,7 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
-  get 'about' => 'pages#about'
-  get 'contact' => 'pages#contact'
-
+ resources :tweets
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
